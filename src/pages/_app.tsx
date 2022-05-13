fe8afdca7a3d81b15d20600baae1fc47ef475999
Daniel Imhoff <dwieeb@gmail.com>
2022-05-12 20:28:53 -0600

    Replace CSS Modules with Stitches

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { globalCss } from '../stitches.config'
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+  globalStyles()
+
   return <Component {...pageProps} />
 }
+
+const globalStyles = globalCss({
+  '*': {
+    boxShadow: 'border-box',
+  },
+  'html, body': {
+    margin: 0,
+    padding: 0,
+    fontFamily: '$body',
+  },
+  'a': {
+    color: 'inherit',
+    textDecoration: 'none',
+  },
+})
 
 export default MyApp
---
+++ src/stitches.config.ts
@@ -0,0 +1,18 @@
+import { createStitches } from '@stitches/react'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  theme: {
+    fonts: {
+      body: '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif',
+    },
+  },
+})
